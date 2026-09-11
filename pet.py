5b4b35026d7b1210d0e3cf34f5c11e5b99115076
apochifavour <apochifavourogwa@gmail.com.com>
2026-09-11 16:24:15 +0100

    practice work

---
+++ pet.py
@@ -0,0 +1,19 @@
+name = input("Enter your name: ")
+print("Hello,", name, "! welcome!")
+
+Dan = 24
+Sam = 20
+my_list = [10, 20, 30, 40, 50]
+
+if (Dan not in my_list):
+    print("Dan is Not present in given list")
+else:
+    print("Dan is present in given list")
+if (Sam in my_list):
+    print("Sam is present in given list")
+else:
+    print("Sam is Not present in given list") 
+
+import keyword 
+print("The list of keyboards are : ")
+print(keyword.kwlist)               
